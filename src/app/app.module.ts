d0dbd7e69cb19a93f1e90a66cc6f825eb93356a4
jkarshin <jtkarshin@gmail.com>
2022-01-13 20:45:30 -0800

    Temp commit with missin files error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,35 @@ import { HelloComponent } from './hello.component';
 import { MyInputComponentComponent } from './my-input-component/my-input-component.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { InventoryDescComponent } from './inventory-desc/inventory-desc.component';
+import { TabBarComponent } from './tab-bar/tab-bar.component';
+import { UIRouterModule } from '@uirouter/angular';
+// import { UIRouterModule } from '@uirouter/angular';
+
+/** States */
+
+const inventoryState = { name: 'inventory', component: InventoryComponent };
+const inventoryDescState = {
+  name: 'inventoryDesc',
+  component: InventoryDescComponent,
+};
+// TODO combined?
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    UIRouterModule.forRoot({
+      states: [inventoryState, inventoryDescState],
+      useHash: true,
+    }),
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
     InventoryComponent,
     InventoryDescComponent,
     MyInputComponentComponent,
+    TabBarComponent,
   ],
   bootstrap: [AppComponent],
 })
